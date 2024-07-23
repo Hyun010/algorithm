cb50f685771989513bd62982a94575e72a2e11ee
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2024-07-23 20:43:14 +0900

    [level 2] Title: 주식가격, Time: 36.24 ms, Memory: 19.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,10 @@
+def solution(prices):
+    l=len(prices) #길이
+    answer = [i for i in range(l-1,-1,-1)] #최댓값으로 초기화, -1은 0까지 하기 위함, -1 전체에서 자기자신 뺴고 앞선 수 뺴기
+    s=[0] #스택 초기화
+    for i in range(1,l): #시간경과
+        while s and prices[s[-1]]>prices[i]: #스택이 존재하며 현재값과 미래값 비교해서 크면
+            j=s.pop() #스택 팝해서 시간 불러오기
+            answer[j]=i-j #현재시간에 시간
+        s.append(i)
+    return answer
