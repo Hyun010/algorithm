8ff3d270148cef93cde1fead3e51056e39593ac6
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2024-11-07 18:40:56 +0900

    [level 0] Title: 소인수분해, Time: 0.82 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,14 @@
+def prime_factors(n):
+    factors = []
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            factors.append(i)
+            n = n / i
+        else:
+            i = i + 1
+    return factors
+
+def solution(n):
+    answer=prime_factors(n)
+    return list(sorted(set(answer)))
