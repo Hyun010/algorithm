960ab16dcfeddc0929b65a7bf1e576e624da8abe
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2024-11-29 18:05:58 +0900

    [level 2] Title: 숫자의 표현, Time: 3.46 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 1
+    for i in range(1,n//2+1):
+        sum=0
+        while sum<n:
+            sum+=i
+            if sum==n:
+                answer+=1
+                break
+            i+=1
+    return answer
