835ada02dc92e5e21e3e99fb5b5922ae913dfb1f
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2025-01-13 17:44:16 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -0,0 +1,9 @@
+def solution(num, total):
+    answer = []
+    cnt=0
+    for i in range(1,num):
+        cnt+=i
+    first=int((total-cnt)/num)
+    for i in range(0,num):
+        answer.append(first+i)
+    return answer
